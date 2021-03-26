b9cd2d3ee51a4501873b4a17b3aaa6c742af8499
James Fraser <jimbob4t@virginmedia.com>
2021-03-26 13:04:30 +0000

    Can't choose same letter more than once

--- src/components/CharacterButton.js
+++ src/components/CharacterButton.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CharacterButton.css';
 const CharacterButton = ({ buttonCharacter, guessedCharacters, setGuessedCharacters }) => {
+	const [ characterAlreadySelected, setCharacterAlreadySelected ] = useState(false);
 	const handleCharacterButtonClick = () => {
 		setGuessedCharacters((guessedCharacters) => [ ...guessedCharacters, buttonCharacter ]);
+		setCharacterAlreadySelected(true);
 	};
-	return <button onClick={() => handleCharacterButtonClick()}>{buttonCharacter}</button>;
+	return (
+		<button onClick={() => handleCharacterButtonClick()} disabled={characterAlreadySelected}>
+			{buttonCharacter}
+		</button>
+	);
 };
 export default CharacterButton;

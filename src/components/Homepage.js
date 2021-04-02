ccc0199e5df2b3532da7a05fe1bdfa09751818fd
James Fraser <jimbob4t@virginmedia.com>
2021-04-02 13:59:53 +0100

    Added another category tvshows and secured the api key

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Homepage.css';
+import tmdbApiKey from '../ApiKey.js';
+
 const Homepage = ({ setBankOfWords }) => {
 	const getCountryNames = (type) => {
 		fetch('https://restcountries.eu/rest/v2/all')
@@ -16,11 +18,27 @@ const Homepage = ({ setBankOfWords }) => {
 			.catch((err) => console.error(err));
 	};
 
+	const getTvShowNames = () => {
+		fetch('https://api.themoviedb.org/3/tv/popular?api_key=' + tmdbApiKey + '&language=en-US&page=1')
+			.then((res) => res.json())
+			.then((data) => {
+				const tvShowNamesArray = [];
+				data.results.forEach((result) => {
+					tvShowNamesArray.push(result.name);
+				});
+				console.log(data);
+				setBankOfWords(tvShowNamesArray);
+				return data;
+			})
+			.catch((err) => console.error(err));
+	};
+
 	return (
 		<div>
 			<p>Categories</p>
 			<button onClick={() => getCountryNames('name')}>Countries</button>
 			<button onClick={() => getCountryNames('capital')}>Capitals</button>
+			<button onClick={() => getTvShowNames()}>T.V.Shows</button>
 		</div>
 	);
 };

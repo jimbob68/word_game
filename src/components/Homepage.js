1efc481aa9c5848df57555ec614aec2177719cdc
James Fraser <jimbob4t@virginmedia.com>
2021-04-08 11:14:37 +0100

    Added a filter to TV shows category to only show British and American shows

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,7 @@ import './Homepage.css';
 import tmdbApiKey from '../ApiKey.js';
 
 const Homepage = ({ setBankOfWords }) => {
-	const getCountryNames = (type) => {
+	const getCountryInfo = (type) => {
 		fetch('https://restcountries.eu/rest/v2/all')
 			.then((res) => res.json())
 			.then((results) => {
@@ -19,12 +19,21 @@ const Homepage = ({ setBankOfWords }) => {
 	};
 
 	const getTvShowNames = () => {
-		fetch('https://api.themoviedb.org/3/tv/popular?api_key=' + tmdbApiKey + '&language=en-US&page=1')
+		const pageNumber = Math.floor(Math.random() * 500) + 1;
+		fetch('https://api.themoviedb.org/3/tv/popular?api_key=' + tmdbApiKey + '&language=en-US&page=' + pageNumber)
 			.then((res) => res.json())
 			.then((data) => {
 				const tvShowNamesArray = [];
 				data.results.forEach((result) => {
-					tvShowNamesArray.push(result.name);
+					if (
+						result.original_language === 'en' &&
+						(result.origin_country[0] === 'US' || result.origin_country[0] === 'GB')
+					) {
+						console.log('popularity', result.popularity);
+						console.log('name', result.name);
+						console.log('origin country', result.origin_country[0]);
+						tvShowNamesArray.push(result.name);
+					}
 				});
 				console.log(data);
 				setBankOfWords(tvShowNamesArray);
@@ -37,9 +46,15 @@ const Homepage = ({ setBankOfWords }) => {
 		<div className="home-container">
 			<p className="home-intro-text">Choose a Category</p>
 			<div className="home-button-container">
-			<button className="home-button" onClick={() => getCountryNames('name')}>Countries</button>
-			<button className="home-button" onClick={() => getCountryNames('capital')}>Capital Cities</button>
-			<button className="home-button" onClick={() => getTvShowNames()}>T.V.Shows</button>
+				<button className="home-button" onClick={() => getCountryInfo('name')}>
+					Countries
+				</button>
+				<button className="home-button" onClick={() => getCountryInfo('capital')}>
+					Capital Cities
+				</button>
+				<button className="home-button" onClick={() => getTvShowNames()}>
+					T.V.Shows
+				</button>
 			</div>
 		</div>
 	);
